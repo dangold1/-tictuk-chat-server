7d8c390793933b57dda12d74ff45f2005a0252e0
dangold1 <dangold308@gmail.com>
2021-03-03 13:42:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const PORT = 8080;
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+});
+
+
+io.on('connection', (socket) => {
+    /* socket object may be used to send specific messages to the new connected client */
+    console.log('new client connected');
+});
